995b472f1db4396564e56dda3884f3507556688c
gmdmgithub <gmika@interia.pl>
2019-01-17 21:12:01 +0100

    maps closures and ranges, plus struct's and interfaces' pointers

---
+++ 04_map_range_pointers/main.go
@@ -0,0 +1,87 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hi there use me")
+	mapFunc()
+	rangeFunc()
+	pointerFunc()
+}
+
+func mapFunc() {
+	//map definition
+	emails := make(map[string]string)
+
+	//key values
+	emails["greg"] = "greg@gmail.com"
+	emails["john"] = "john@gmail.com"
+
+	fmt.Println(len(emails))
+	fmt.Println("Adam email is ", emails["adam"]) //wrong?
+	fmt.Println("John email is ", emails["john"])
+
+	if len(emails["adam"]) == 0 {
+		emails["adam"] = "adam@gmail.com"
+	}
+	fmt.Println(emails)
+
+	//delete one element
+	delete(emails, "adam")
+
+	fmt.Println(emails)
+
+	//new map defined at creation
+	scores := map[string]int{"adam": 20, "mark": 28, "greg": 60}
+	scores["john"] = 11
+	fmt.Println(scores)
+
+}
+
+func rangeFunc() {
+	fmt.Println("##### range func and loops ####")
+	someNum := []int{21, 34, 11, 23, 45, 63}
+
+	//range with index
+	for i, id := range someNum {
+		fmt.Printf("%d - num %d\n", i, id)
+	}
+
+	//range withot index
+	for id := range someNum {
+		fmt.Printf("num %d\n", id)
+	}
+	//range withot index different way
+	for _, id := range someNum {
+		fmt.Printf("num with _ %d\n", id)
+	}
+
+	fmt.Println(" standard loop ")
+	for j := 0; j < len(someNum); j++ {
+		fmt.Printf("%d - num %d\n", j, someNum[j])
+	}
+
+	//range with map
+	scores := map[string]int{"adam": 20, "mark": 28, "greg": 60}
+	for key, value := range scores {
+		fmt.Printf("Key is %s, and value is %d\n", key, value)
+	}
+
+}
+
+func pointerFunc() {
+	fmt.Println("##### Pointers ####")
+	a := 5
+	b := &a
+
+	fmt.Println(a, b)
+	fmt.Printf("%T\n", b)
+
+	//read val from address
+	fmt.Println(*b)
+	fmt.Println(*&a)
+
+	//change valu with pointer
+	*b = 10
+	fmt.Println(a)
+}
---
+++ 07_struct/main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+// Define person struct
+type Person struct {
+	firstName, lastName string
+	city                string
+	gender              string
+	age                 int
+}
+
+//value reciever
+func (person Person) sayHello() string {
+	return "Hi there it's " + person.firstName + " and my age is " + strconv.Itoa(person.age)
+}
+
+//pointer reciever
+func (p *Person) addYear() {
+	p.age++
+}
+
+//interface
+type Vehicle interface {
+	nweels() int
+}
+
+type Car struct {
+	name  string
+	weels int
+}
+
+type Bike struct {
+	weels   int
+	name    string
+	handars bool
+}
+
+func (c Car) nweels() int {
+	return c.weels
+}
+
+func (b Bike) nweels() int {
+	return b.weels
+}
+
+func getWeels(v Vehicle) int {
+	return v.nweels()
+}
+
+func main() {
+	fmt.Println("Hi there!")
+	person := Person{firstName: "Anna", lastName: "Smith", city: "NY", gender: "f", age: 27}
+	person2 := Person{"Adam", "Jordan", "LA", "m", 43}
+	fmt.Println(person)
+	fmt.Println(person2)
+
+	fmt.Println(person.city)
+	person2.age++
+	fmt.Println(person2)
+
+	fmt.Println(person2.sayHello())
+	person.addYear()
+	fmt.Println(person)
+
+	volvo := Car{"VXC", 4}
+	mtb := Bike{2, "Bike", true}
+
+	fmt.Printf("Car has %d weels\n", getWeels(volvo))
+	fmt.Printf("Bike has %d weels\n", getWeels(mtb))
+}
