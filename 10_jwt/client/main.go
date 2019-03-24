670fc80ca3d7b1de7df6ba697a7ed9dffbadea16
gmdmgithub <gmika@interia.pl>
2019-03-24 22:58:21 +0100

    token jwt working with client and server

--- 10_jwt/client/main.go
+++ 10_jwt/client/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"time"
@@ -18,7 +20,7 @@ func GenerateJWT() (string, error) {
 	claimes := token.Claims.(jwt.MapClaims)
 
 	claimes["authorized"] = true
-	claimes["client"] = "Alex"
+	claimes["client"] = "Alex Smith"
 	claimes["exp"] = time.Now().Add(time.Minute * 30).Unix()
 
 	tokenString, err := token.SignedString(mySignedKey)
@@ -33,13 +35,44 @@ func GenerateJWT() (string, error) {
 
 func homePage(w http.ResponseWriter, r *http.Request) {
 
-	log.Printf("Client get request /")
+	log.Printf("Client got request")
 
 	tokenValue, err := GenerateJWT()
 	if err != nil {
 		fmt.Fprintf(w, fmt.Sprintf("crash ...., %v\n", err))
 	}
-	fmt.Fprintf(w, "got token %s", tokenValue)
+	respMap := make(map[string]string)
+	respMap["token"] = tokenValue
+	jsonString, err := json.Marshal(respMap)
+	if err != nil {
+		log.Println("cannot convert to JSON")
+		fmt.Fprintf(w, "Error: %s", err.Error())
+	}
+	// fmt.Fprintf(w, string(jsonString))
+	log.Println(string(jsonString))
+
+	client := &http.Client{}
+
+	req, err := http.NewRequest("GET", "http://localhost:8001", nil)
+	if err != nil {
+		fmt.Fprintf(w, "Error: %s", err.Error())
+	}
+	req.Header.Set("Token", tokenValue)
+
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Fprintf(w, "Error: %s", err.Error())
+	}
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println(err)
+		fmt.Fprintf(w, "Error: %s", err.Error())
+	}
+
+	//finally replay
+	fmt.Fprintf(w, string(body))
+
 }
 
 func handleRequest() {
--- 10_jwt/server/main.go
+++ 10_jwt/server/main.go
@@ -4,16 +4,52 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	jwt "github.com/dgrijalva/jwt-go"
 )
+
+var mySignedKey = []byte("testmeifyoucanorjustuseown")
 
 func mainPage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Secret info!!")
 }
 
+// isAuthorized - check if request is authorized
+func isAuthorized(endpoinToCkeck func(http.ResponseWriter, *http.Request)) http.Handler {
+	fmt.Println("Autorizition check is working ...")
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		log.Printf("Server retriving ...")
+
+		if r.Header["Token"] != nil {
+
+			token, err := jwt.Parse(r.Header["Token"][0], func(token *jwt.Token) (interface{}, error) {
+				if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+					return nil, fmt.Errorf("There was an error ")
+				}
+				return mySignedKey, nil
+			})
+
+			if err != nil {
+				fmt.Fprintf(w, err.Error())
+			}
+			if token.Valid {
+				endpoinToCkeck(w, r)
+			}
+		} else {
+			fmt.Fprint(w, "Not authorized!")
+		}
+	})
+
+}
+
 // handleRequests - hendle all requests
 func handleRequests() {
 
-	http.HandleFunc("/", mainPage)
+	// http.HandleFunc("/", mainPage)
+	// after introduction middleware use isAuthorized
+
+	http.Handle("/", isAuthorized(mainPage))
 
 	log.Fatal(http.ListenAndServe(":8001", nil))
 }
