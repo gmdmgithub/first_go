9496d491785db7ba9fc0307eeb7792a597668b82
gmdmgithub <gmika@interia.pl>
2019-03-27 08:49:51 +0100

    encryption and decription with ADS

--- 13_encrypt_decrypt/main.go
+++ 13_encrypt_decrypt/main.go
@@ -1,16 +1,87 @@
 package main
 
 import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"errors"
 	"fmt"
+	"io"
+	"log"
 )
 
-func encrypt(data []byte, secretPhrase string) []byte {
+func encrypt(data, secretKey []byte) ([]byte, error) {
 
-	return nil
+	// lets wrap it in Galois Counter Mode (GCM) with a standard nonce length
+	gcm, err := getGCM(secretKey)
+	if err != nil {
+		return nil, err
+	}
+
+	nonce := make([]byte, gcm.NonceSize())
+
+	if _, err = io.ReadFull(rand.Reader, nonce); err != nil {
+		return nil, err
+	}
+
+	return gcm.Seal(nonce, nonce, data, nil), nil
+
 }
+
+func decrypt(ciphertext []byte, secretKey []byte) ([]byte, error) {
+
+	// lets wrap it in Galois Counter Mode (GCM) with a standard nonce length
+	gcm, err := getGCM(secretKey)
+	if err != nil {
+		return nil, err
+	}
+
+	nonceSize := gcm.NonceSize()
+
+	if len(ciphertext) < nonceSize {
+		return nil, errors.New("ciphertext too short")
+	}
+
+	nonce, ciphertext := ciphertext[:nonceSize], ciphertext[nonceSize:]
+	return gcm.Open(nil, nonce, ciphertext, nil)
+}
+
+// getGCM - stands from Galois Counter Mode (GCM) - provides Authenticated Encryption with Associated Data (AEAD)
+func getGCM(secretKey []byte) (cipher.AEAD, error) {
+
+	//lets create a new block cipher based on the secretKey
+	myCipher, err := aes.NewCipher(secretKey)
+	//it could be also based on hashed secret key
+	// myCipher,err := aes.NewCipher(createHash(passphrase)) //- from the previous example => 12_hashing
+	if err != nil {
+		return nil, err
+	}
+	// lets wrap it in Galois Counter Mode (GCM) with a standard nonce length
+	gcm, err := cipher.NewGCM(myCipher)
+	if err != nil {
+		return nil, err
+	}
+	return gcm, nil
+}
+
+// secretKey (key) parameter must be 16, 24 or 32, corresponding to the AES-128, AES-192 or AES-256 algorithms, respectively
+var secretKey = []byte("this-is-the-most-secret-key-ever")
 
 func main() {
 
 	fmt.Println("welcome to the encrypt decrypt playground, the most opinioated AES (Advanced Encription Standard) is used")
 
+	textToEnrypt := []byte("My name is Alex Smith - it's top secret information!")
+
+	ciphertext, err := encrypt(textToEnrypt, secretKey)
+	if err != nil {
+		log.Fatal(err) //TODO - do something with this error
+	}
+	fmt.Printf("Planin text is: %s encrypted: %x\n", textToEnrypt, ciphertext)
+	// ### decrypting
+	plaintext, err := decrypt(ciphertext, secretKey)
+	if err != nil {
+		log.Fatal(err) // TODO: Properly handle error
+	}
+	fmt.Printf("Decrypted text: %x and encrypted is: %s\n", ciphertext, plaintext)
 }
