ee4f504010ca5cc70e56293eb0e75ae0cb84f15c
gmdmgithub <gmika@interia.pl>
2019-03-28 15:34:07 +0100

    infinite loop for channel

---
+++ 15_channels/infinite.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func putToChannel(c chan int, num int) {
+
+	for i := 0; i < num; i++ {
+		c <- i * 2
+		time.Sleep(100 * time.Millisecond)
+	}
+
+	close(c)
+}
+
+// procesInfiniteWait - example how to use
+func procesInfiniteWait() {
+	fmt.Println("Welcome to the infinite loop with channels")
+
+	c := make(chan int)
+
+	go putToChannel(c, 20)
+
+	//waiting for answers ...
+	for {
+		val, ok := <-c
+		if ok == false {
+			fmt.Println("Channel is closed :(")
+			break
+		} else {
+			fmt.Printf("We get the channel value: %d\n", val)
+		}
+	}
+
+	fmt.Println("By by from infinite loop with channels")
+}
--- 15_channels/simple.go
+++ 15_channels/simple.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 // greet - just present greeting - but from channel
@@ -14,13 +15,40 @@ func greet(c chan string) {
 func channelSample() {
 
 	//to be effective channels have to be created be make
-	strinChannel := make(chan string)
+	stringChannel := make(chan string)
 
 	// if you do not create a goroutine an error will occur - comment the line below to check
-	go greet(strinChannel)
-	defer close(strinChannel)
+	go greet(stringChannel)
+	defer close(stringChannel)
 	// time.Sleep(10 * time.Millisecond)
-	strinChannel <- "Alex"
+	stringChannel <- "Alex"
 
-	fmt.Println("By from channel")
+	fmt.Println("By from simple channel!\n############")
+}
+
+func greetMany(number int, c chan string) {
+
+	for i := 0; i < number; i++ {
+		fmt.Printf("Hi from blocking channel: %s\n", <-c)
+	}
+}
+
+func chcekBlocking() {
+
+	c := make(chan string)
+
+	go greetMany(3, c)
+	fmt.Println("Lte's send Alex")
+	c <- "Alex"
+	fmt.Println("Lte's send Marry")
+	c <- "Marry"
+	// go close(c)
+
+	fmt.Println("Lte's send Kriss - wait 1 sek ;-)")
+	time.Sleep(1 * time.Second)
+	c <- "Kriss"
+
+	time.Sleep(10 * time.Millisecond)
+
+	fmt.Println("By from channel blocking!!")
 }
