9c9d6501a1e7611994cc9ad347cca995cc69951d
gmdmgithub <gmika@interia.pl>
2019-04-04 12:44:11 +0200

    sematphore with chanel ...

---
+++ 17_semaphore/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"time"
+)
+
+var countetr = 1 // manipulate paralel namber of executions
+
+var semaphoreChan = make(chan struct{}, countetr)
+
+func doTheJob(index int) {
+
+	semaphoreChan <- struct{}{} //block while its full
+
+	go func() {
+		defer func() {
+			<-semaphoreChan //read to release
+		}()
+		log.Println(index)
+		time.Sleep(3 * time.Second) // simulate long lasting task
+	}()
+
+}
+
+func main() {
+	fmt.Println("Hi semaphore here ----------")
+	defer fmt.Println("Bye semaphore  ----------")
+
+	for i := 0; i < 20; i++ {
+		doTheJob(i)
+	}
+}
