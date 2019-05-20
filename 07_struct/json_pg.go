91b39b13e816703fec45d4e7cb364b54d1df87e9
gmdmgithub <gmika@interia.pl>
2019-05-20 12:18:56 +0200

    json endode buffer and marshal

--- 07_struct/json_pg.go
+++ 07_struct/json_pg.go
@@ -3,40 +3,60 @@ package main
 import (
 	"bytes"
 	"encoding/json"
+	"io"
 	"log"
 )
 
-type User struct {
-	name     string
-	password string
-	age      int
-	salary   float32
-	manager  bool
+type User struct { //TODO!! - add more and remember Capitalize first letter!!
+	Name     string `json:"name"`
+	Password string `json:"password"`
+	Age      int
 }
 
 type Users []User
 
+// The method takes just a io.Writer as input
+func (p *User) Write(w io.Writer) {
+	b, err := json.Marshal(*p)
+	if err != nil {
+		log.Printf("problem with marchaling %v", err)
+	}
+	// Inside our function we just write into the io.Writer
+	// We don't care about which writer we use
+	w.Write(b)
+}
+
 func simpleJSON() {
 
 	usr := User{
-		name:     "Alex",
-		password: "secret",
-		age:      27,
-		salary:   120000.00,
-		manager:  false,
+		Name:     "Alex",
+		Password: "secret",
+		Age:      27,
 	}
 
-	log.Printf("User is v%", usr)
+	log.Printf("User is %v", usr)
+	// var buf = new(bytes.Buffer)
 
-	var buf = new(bytes.Buffer)
+	var b bytes.Buffer // A Buffer needs no initialization.
 
-	err := json.NewEncoder(buf).Encode(usr)
-	if err != nil {
-		log.Fatal("problem with marchaling ", err)
-	}
-
-	log.Printf("Encodeing result %v", buf)
+	usr.Write(&b)
+	log.Printf("Buffer is %s", b.String())
 
 	// io.Copy(os.Stdout, buf)
 
+}
+
+func withBuffer() {
+	log.Println("Buffer and Encode way")
+	usr := User{
+		Name:     "Kris",
+		Password: "1111",
+		Age:      31,
+	}
+	var buf = new(bytes.Buffer)
+
+	if err := json.NewEncoder(buf).Encode(usr); err != nil {
+		log.Printf("Problem with decode %v", err)
+	}
+	log.Printf("%s", buf)
 }
--- 07_struct/main.go
+++ 07_struct/main.go
@@ -133,4 +133,5 @@ func main() {
 	// mainPlayground()
 	inheritance()
 	simpleJSON()
+	withBuffer()
 }
