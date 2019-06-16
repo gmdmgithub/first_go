b05f4a8b56e3b57007f9060addd7551922ff4601
gmdmgithub <gmika@interia.pl>
2019-06-16 13:33:00 +0200

    docker file added to http playground

--- 04_http/main.go
+++ 04_http/main.go
@@ -4,23 +4,31 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"strconv"
 )
 
 // http.ResponseWriter assembles the servers response and writes to
 // the client
 // http.Request is the clients request
 func handler(w http.ResponseWriter, r *http.Request) {
+	log.Println("### Home page")
+	defer log.Println("### Home page bye")
 
 	// Writes to the client
 	fmt.Fprintf(w, "Hi there we are with you and using Go!\n")
 }
 
 func handler2(w http.ResponseWriter, r *http.Request) {
+	log.Println("### getting users")
+	defer log.Println("### getting users bye")
 	users := []string{"Mark", "Frank", "John"}
+
 	fmt.Fprintf(w, "Hello on Earth\n")
 	for i := 0; i < len(users); i++ {
 		fmt.Fprintln(w, users[i])
+	}
+
+	for index, usr := range users {
+		fmt.Fprintln(w, fmt.Sprintf("index: %d value: %v", index, usr))
 	}
 }
 
@@ -28,7 +36,7 @@ func handlerOnce() http.HandlerFunc {
 	log.Println("This code will execute only once!")
 
 	return func(w http.ResponseWriter, r *http.Request) {
-		log.Println("This code is like clousers")
+		log.Println("This code is like a closure func")
 		fmt.Fprintf(w, "Hi there - back from func")
 	}
 }
@@ -46,7 +54,8 @@ func main() {
 
 	port := 8081
 	// Listen to port (8081) and handle requests - response is nil (null)
-	fmt.Println("Server is running on port " + strconv.Itoa(port) + " ...")
-	http.ListenAndServe(":"+strconv.Itoa(port), nil)
+	fmt.Printf("Server is running on port %d ...", +port)
+	// +strconv.Itoa
+	http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
 
 }
